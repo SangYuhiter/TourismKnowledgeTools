1bce254ec3a32656996b3d530fcc2ab32fcb289c
SangyYuhiter <sangyu.code@gmail.com>
2023-05-23 22:30:11 +0800

    change format and line height

--- ParseData5-8.py
+++ ParseData5-8.py
@@ -24,11 +24,16 @@ TRAVELDETAIL_KEY_WORD = "traveldetail"
 SCENIC_SHEET_NAME = "scenic"
 TRAVELDETAIL_SHEET_NAME = "td"
 
+SHEET_FONT_NAME = "宋体"
+SHEET_FONT_SIZE = 20 * 11
+SHEET_LINE_HEIGHT = (int)(20 * 14.4)
+
+
 def parse_data(input_file_path):
     print("process input:{}".format(input_file_path))
     # use set for remove duplicate record
     all_lines = set()
-    with open(input_file_path,"r",encoding=INPUT_ENCODING) as fr:
+    with open(input_file_path, "r", encoding=INPUT_ENCODING) as fr:
         # remove line 0
         for line in fr.readlines()[1:]:
             for record in line.split(";"):
@@ -46,24 +51,33 @@ def parse_data(input_file_path):
         elif TRAVELDETAIL_KEY_WORD in line:
             travel_detail_records.append(line)
         else:
-            print("can not cover line:{}",line)
+            print("can not cover line:{}", line)
 
     for line in all_lines:
         # print(line)
         pass
-    print("all record size:{},scenic record size:{},traveldetail record size:{}".format(len(all_lines),len(scenic_records),len(travel_detail_records)))
+    print("all record size:{},scenic record size:{},traveldetail record size:{}".format(len(all_lines),
+                                                                                        len(scenic_records),
+                                                                                        len(travel_detail_records)))
 
     # write info to xlsx
-    output_file_path = input_file_path.replace(INPUT_FORMAT,OUTPUT_FORMAT)
+    output_file_path = input_file_path.replace(INPUT_FORMAT, OUTPUT_FORMAT)
     print("output_file_path:{}".format(output_file_path))
     work_book = xlwt.Workbook(encoding=OUTPUT_ENCODING)
     travel_detail_sheet = work_book.add_sheet(TRAVELDETAIL_SHEET_NAME)
     scenic_sheet = work_book.add_sheet(SCENIC_SHEET_NAME)
+    style = xlwt.XFStyle()
+    font = xlwt.Font()
+    font.name = SHEET_FONT_NAME
+    font.height = SHEET_FONT_SIZE
+    style.font = font
     # bypass head line,just save data
     for i in range(len(travel_detail_records)):
-        travel_detail_sheet.write(i,0,travel_detail_records[i])
+        travel_detail_sheet.row(i).height = SHEET_LINE_HEIGHT
+        travel_detail_sheet.write(i, 0, travel_detail_records[i], style)
     for i in range(len(scenic_records)):
-        scenic_sheet.write(i,0,scenic_records[i])
+        scenic_sheet.row(i).height = SHEET_LINE_HEIGHT
+        scenic_sheet.write(i, 0, scenic_records[i], style)
     work_book.save(output_file_path)
     print("output_file_path:{} save successful!!!".format(output_file_path))
 
@@ -72,7 +86,6 @@ if __name__ == "__main__":
     # demo path
     for file in os.listdir(SOURCE_DATA_PATH):
         if INPUT_FORMAT in file:
-            parse_data(os.path.join(SOURCE_DATA_PATH,file))
+            parse_data(os.path.join(SOURCE_DATA_PATH, file))
     input("请按任意键退出......")
     pass
-
